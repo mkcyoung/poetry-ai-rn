268c79b3752e2992a4de973dde4aae0b1b8a6c7f
mkcyoung <m.c.young0@gmail.com>
2023-06-10 14:29:48 -0600

    Eslint, prettier, simulator setup, and more

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+
+const COLORS = {
+  white: "#fff",
+}
 
 export default function App() {
   return (
@@ -7,14 +11,14 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: COLORS.white,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
-});
+})
